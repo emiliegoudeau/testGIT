5d948ba24a190c1002ee8b4e98139a4e1b315571
Adouairy <adrien.ouairy@wanadoo.fr>
2017-04-03 14:47:53 +0100

    Mise a jour de ton profil 03/04/2017 14h47 "pleins de belles modifications"

--- test_GIT2/src/test_GIT2/Emilie.java
+++ test_GIT2/src/test_GIT2/Emilie.java
@@ -2,4 +2,10 @@ package test_GIT2;
 
 public class Emilie {
 
+	protected String nom = "Goudeau";
+	protected String prenom = "Emilie";
+
+	protected String presentationEmilie() {
+		return "Bonjour je m'appelle " + prenom + " " + nom;
+	}
 }
