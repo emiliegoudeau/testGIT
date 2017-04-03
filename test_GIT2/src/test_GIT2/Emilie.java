73329f8c51e0afd3b8b258cd1f753b8dda11c851
Adouairy <adrien.ouairy@wanadoo.fr>
2017-04-03 14:59:11 +0100

    modification resolution conflit

--- test_GIT2/src/test_GIT2/Emilie.java
+++ test_GIT2/src/test_GIT2/Emilie.java
@@ -1,17 +1,6 @@
 package test_GIT2;
 
 public class Emilie {
-	
-	protected String nom = "GOUDEAU";
-	protected String prenom = "Emilie";
-	
-	
-	public String getPresentation() {
-		return "Je m'appelle " + nom + " " + prenom;
-		
-	}
-
-	
 
 	protected String nom = "Goudeau";
 	protected String prenom = "Emilie";
