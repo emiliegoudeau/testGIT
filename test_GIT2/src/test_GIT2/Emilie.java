4fb2bdda04b9d502d177a71a7c68b0a93bac9296
Emilie GOUDEAU <emiliegoudeau.eg@gmail.com>
2017-04-03 14:46:51 +0200

    Mise à jour du 03/04/2017 J'ai mis un commentaire

--- test_GIT2/src/test_GIT2/Emilie.java
+++ test_GIT2/src/test_GIT2/Emilie.java
@@ -1,5 +1,16 @@
 package test_GIT2;
 
 public class Emilie {
+	
+	protected String nom = "GOUDEAU";
+	protected String prenom = "Emilie";
+	
+	
+	public String getPresentation() {
+		return "Je m'appelle " + nom + " " + prenom;
+		
+	}
+
+	
 
 }
